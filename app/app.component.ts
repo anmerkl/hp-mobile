5aefffa139e4692a7bc675c98d6f0e0fd5235059
Alfonso Merkl <anmerkl@gmail.com>
2017-11-14 09:04:48 -0500

    removed scaffolding, added basic login screen

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,15 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { User } from './models/user/user';
 
 @Component({
-    selector: "ns-app",
-    templateUrl: "app.component.html",
+    selector: 'login',
+    templateUrl: './pages/login/login.component.html',
 })
 
-export class AppComponent { }
+export class AppComponent {
+  user: User;
+
+  constructor() {
+
+  }
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -3,12 +3,8 @@ import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { AppRoutingModule } from "./app.routing";
 import { AppComponent } from "./app.component";
 
-import { ItemService } from "./item/item.service";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
-
 // Uncomment and add to NgModule imports if you need to use two-way binding
-// import { NativeScriptFormsModule } from "nativescript-angular/forms";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
 
 // Uncomment and add to NgModule imports  if you need to use the HTTP wrapper
 // import { NativeScriptHttpModule } from "nativescript-angular/http";
@@ -19,15 +15,16 @@ import { ItemDetailComponent } from "./item/item-detail.component";
     ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule
+        AppRoutingModule,
+        NativeScriptFormsModule
     ],
     declarations: [
         AppComponent,
-        ItemsComponent,
-        ItemDetailComponent
+        //ItemsComponent,
+        //ItemDetailComponent
     ],
     providers: [
-        ItemService
+        //ItemService
     ],
     schemas: [
         NO_ERRORS_SCHEMA
